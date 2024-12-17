46b119835240db5cd36fd427302a94d97ccc35b2
FateNier <green_too@126.com>
2024-12-17 11:21:01 +0800

    FateNier-add-tech-spbot3

---
+++ spbot3/src/main/java/com/tech/spbot3/Spbot3Application.java
@@ -0,0 +1,13 @@
+package com.tech.spbot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spbot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spbot3Application.class, args);
+    }
+
+}
